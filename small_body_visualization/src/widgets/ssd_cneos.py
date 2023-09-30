2e5c98b7db14fa1daf49dbbe648cc1b2f0cebb10
Space Nerd <florianpospiech77@gmail.com>
2023-09-30 11:34:05 +0200

    remove old code

--- small_body_visualization/src/widgets/ssd_cneos.py
+++ small_body_visualization/src/widgets/ssd_cneos.py
@@ -43,26 +43,3 @@ class SSD_CNEOS(tk.Frame):
                 e.insert(0, self.df.iloc[r, c])
                 e.grid(row=r+1, column=c)
                 e.bind('<KeyRelease>', lambda event, y=r, x=c: self.data_processing(event, y,x))
-        
-        '''
-        canvas = tk.Canvas(self.root, scrollregion="0 0 2000 1000", width=400, height=400)
-        canvas.grid(row=0, column=0, sticky=tk.NSEW)
-
-        scroll = tk.Scrollbar(self.root, orient=tk.VERTICAL, command=canvas.yview)
-        scroll.grid(row=0, column=1, sticky=tk.NS)   
-        canvas.config(yscrollcommand =scroll.set)
-        
-        for fila in range(20):
-            for col in range(5):
-                btn = tk.Button(self.frame, text = f"{fila}-{col}")
-                btn.grid(row = fila, column = col, sticky = tk.NSEW)
-        '''
-        '''
-        for row in range(len(self.df)):
-            for column in range(len(self.df.columns)):
-                e = tk.Entry(self.frame, fg='blue', font=('Arial', 16, 'bold'))
-                e.grid(row=row, column=column, sticky=tk.NSEW)
-        '''
-        '''
-        item = canvas.create_window(( 2, 2 ), anchor = tk.NW,  window = self.frame )
-        '''
